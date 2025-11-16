fdda9df294b1cdf96685584efc258f35cab730b6
Rosa <101842936+rosa1n@users.noreply.github.com>
2025-11-16 09:12:49 -0800

    How the fuck

--- src/main/java/org/shondo/nini/Nini.java
+++ src/main/java/org/shondo/nini/Nini.java
@@ -3,6 +3,7 @@ package org.shondo.nini;
 import monster.psyop.client.plugins.JavaAddon;
 import org.shondo.nini.modules.ExampleModule;
 
+public class Nini implements JavaAddon {
     @Override
     public void onInit() {
         new ExampleModule().load();
